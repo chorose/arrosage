7ce5253a144866949b2f8f3f7bbdf4cf28ca1621
chorose <chorose@yahoo.fr>
2016-02-07 10:59:33 +0100

    montage1

---
+++ sensor.ino
@@ -0,0 +1,22 @@
+#include <RCSwitch.h>
+
+RCSwitch mySwitch = RCSwitch();
+
+// pin analog moisture sensor A2
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
+  // Transmitter is connected to Arduino Pin #13
+  mySwitch.enableTransmit(13);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int sensorValue = analogRead(A2);
+  mySwitch.send(sensorValue, 16);
+  delay(1000);  
+  Serial.println(sensorValue);
+}
