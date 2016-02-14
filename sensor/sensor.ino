57c6c62e270ede454e6ef567d362cf9957a8e28c
chorose <chorose@yahoo.fr>
2016-02-14 12:44:34 +0100

    montage OK avec attiny

--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -1,22 +1,26 @@
+#define __AVR_ATtinyX5__
 #include <RCSwitch.h>
 
 RCSwitch mySwitch = RCSwitch();
 
-// pin analog moisture sensor A2
+// pin analog moisture sensor A1
+int analogInPin = A1;
+// pin digital transmitter 0
+int transmitterPin = 0;
+// pin digital output
+int digitalOutPin = 1;
 
 void setup() {
   // put your setup code here, to run once:
-  //Serial.begin(9600);
-  
-  // Transmitter is connected to Arduino Pin #13
-  mySwitch.enableTransmit(13);
-
+  pinMode(analogInPin, INPUT);
+  //pinMode(transmitterPin, OUTPUT);
+  //pinMode(digitalOutPin, OUTPUT);
+  mySwitch.enableTransmit(transmitterPin);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int sensorValue = analogRead(A2);
+  int sensorValue = analogRead(analogInPin);
   mySwitch.send(sensorValue, 16);
   delay(1000);  
-  //Serial.println(sensorValue);
 }
