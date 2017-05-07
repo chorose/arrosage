1e720861d7658d03186bcd5397e898a9b1eca797
Roselyne Chotin-Avot <Roselyne.Chotin-Avot@lip6.fr>
2017-05-07 14:59:04 +0200

    Simple lecteurs/affichage du capteur

---
+++ Arduino/sensor_simple/sensor_simple.ino
@@ -0,0 +1,15 @@
+int sensorPin = A0;
+int sensorValue = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(sensorPin, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  sensorValue = analogRead(sensorPin);
+  delay(10);
+  Serial.println(sensorValue);
+}
