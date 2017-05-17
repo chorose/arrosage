612748f1a82f164cf880bcf185f80ff0cebbdf6c
Roselyne Chotin-Avot <Roselyne.Chotin-Avot@lip6.fr>
2017-05-17 17:12:33 +0200

    Alimentation des composants via le port 3 de l’attiny (arduino digital 4) Mode débug avec une led sur le port 2 de l’attiny (arduino digital 3)

--- Arduino/prise/prise.ino
+++ Arduino/prise/prise.ino
@@ -5,12 +5,26 @@
 
 RCSwitch mySwitch = RCSwitch();
 
+#define DEBUG
+
 // pin analog moisture sensor A1
 int analogInPin = A1;
 // pin digital transmitter 0
 int transmitterPin = 0;
+// Alimentation des composants
+int alimPin = 4;
+
+#ifdef DEBUG 
+int debug = 3;
+#define DEBUG_ON digitalWrite(debug,HIGH);
+#define DEBUG_OFF digitalWrite(debug,LOW);
+#else
+#define DEBUG_ON
+#define DEBUG_OFF
+#endif
+
 int watchdog_counter = 0;
-int seuil = 500;
+int seuil = 800;
 
 void setup_watchdog(int ii){
   // 0=16ms, 1=32ms,2=64ms,3=128ms,4=250ms,5=500ms
@@ -33,6 +47,10 @@ void setup_watchdog(int ii){
 void setup() {
   // put your setup code here, to run once:
   pinMode(analogInPin, INPUT);
+  pinMode(alimPin, OUTPUT);
+  #ifdef DEBUG
+  pinMode(debug, OUTPUT);
+  #endif
   pinMode(transmitterPin, OUTPUT);
   mySwitch.enableTransmit(transmitterPin);
   //setup_watchdog(9); //Setup watchdog to go off after 8 sec
@@ -50,14 +68,27 @@ void arroser(int sensorValue) {
   int nb = 0;
   mySwitch.send(sensorValue, 16);
   if (sensorValue < seuil) {
-    nb = 5 - (sensorValue / 100);
+    nb = (seuil - sensorValue) / 100;
     for(short i=0;i< nb;i++){
+      DEBUG_ON
       mySwitch.switchOn("10001", 1);
       delay(60000); // 1 min
+      DEBUG_OFF
       mySwitch.switchOff("10001", 1);
       delay(60000); // 1 min
     }
   }
+  DEBUG_OFF
+}
+
+void allumer(){
+  digitalWrite(alimPin,HIGH);
+  delay(30000); // 30 sec
+}
+
+void eteindre(){
+  digitalWrite(alimPin,LOW);
+  delay(30000); // 30 sec
 }
 
 void loop() {
@@ -66,9 +97,11 @@ void loop() {
   //if (watchdog_counter > 30) {
   //watchdog_counter = 0;
   //ADCSRA |= (1 << ADEN); //Enable ADC
+  allumer();
   int sensorValue = analogRead(analogInPin);
   //Serial.println(sensorValue);
   arroser(sensorValue);
+  eteindre();
   //ADCSRA &= ~(1 << ADEN); //Disable ADC, saves ~230uA
   //}
 }
